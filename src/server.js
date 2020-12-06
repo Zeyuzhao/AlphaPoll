5fd4d79180abc7cb4f02da90ec54594f91d615aa
Simon Chervenak <sarras305@gmail.com>
2020-12-05 20:15:37 -0500

    add signin/up functions

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ db.mongoose
     })
     .then(() => {
         console.log("Successfully connect to MongoDB.");
-        initial();
+        // initial();
     })
     .catch(err => {
         console.error("Connection error", err);
