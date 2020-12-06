9ee3744d0e4c9b3d2ddaf2021db4ab10b1b1c0a2
Simon Chervenak <sarras305@gmail.com>
2020-12-05 23:12:19 -0500

    add react-router-dom

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -6,12 +6,29 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import axios from 'axios';
 
+import {
+    BrowserRouter, Route, Link
+} from "react-router-dom";
+import Dashboard from "../dashboard/dashboard";
+
 export default class Login extends Component {
     constructor(props) {
         super(props);
         this.state = { email: "", password: ""}
         this.handleSubmit = this.handleSubmit.bind(this);
+
+        // verifyUser();
     }
+
+    // verifyUser() {
+    //     axios({
+    //         method: 'post',
+    //         url: 'http://localhost:5000/user/verify',
+    //         data: {
+    //             token: // cookie
+    //         }
+    //     })
+    // }
 
     validateForm() {
         return this.state.email.length > 0 && this.state.password.length > 0;
@@ -41,36 +58,40 @@ export default class Login extends Component {
 
     render() {
         return (
-            <div className="Login">
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group size="lg" controlId="email">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control
-                            autoFocus
-                            type="text"
-                            onChange={(e) => this.setState({ name: e.target.value })}
-                        />
-                    </Form.Group>
-                    <Form.Group size="lg" controlId="email">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control
-                            autoFocus
-                            type="email"
-                            onChange={(e) => this.setState({ email: e.target.value })}
-                        />
-                    </Form.Group>
-                    <Form.Group size="lg" controlId="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                            type="password"
-                            onChange={(e) => this.setState({ password: e.target.value })}
-                        />
-                    </Form.Group>
-                    <Button block size="lg" type="submit" disabled={!this.validateForm()}>
-                        Login
-                    </Button>
-                </Form>
-            </div>
+            <BrowserRouter>
+                <div className="Login">
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Group size="lg" controlId="email">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control
+                                autoFocus
+                                type="text"
+                                onChange={(e) => this.setState({ name: e.target.value })}
+                            />
+                        </Form.Group>
+                        <Form.Group size="lg" controlId="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                autoFocus
+                                type="email"
+                                onChange={(e) => this.setState({ email: e.target.value })}
+                            />
+                        </Form.Group>
+                        <Form.Group size="lg" controlId="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                onChange={(e) => this.setState({ password: e.target.value })}
+                            />
+                        </Form.Group>
+                        <Button block size="lg" type="submit" disabled={!this.validateForm()}>
+                            Login
+                        </Button>
+                    </Form>
+                </div>
+
+                <Route path="/dashboard" component={Dashboard}> </Route>
+            </BrowserRouter>
         )
     }
 }
