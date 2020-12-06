d897c5b3341a776d41b41a91c08fd3faafa1b465
Simon Chervenak <sarras305@gmail.com>
2020-12-05 22:11:23 -0500

    move functions to users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,96 @@
+const jwt = require("jsonwebtoken");
+const config = require("src/models/config.js");
+const bcrypt = require("bcryptjs");
+
 const express = require('express');
 const router = express.Router();
 
-router.get('/login', (req, res) => res.send('login'));
+const User = require('../models/User.model.js');
 
-router.get('/register', (req, res) => res.send('register'));
+check_duplicate = (email, callback, fail) => {
+    User.findOne({
+        email: email
+    }).exec((err, user) => {
+        if (err) {
+            fail(err);
+        }
+
+        if (user) {
+            fail("Failed! Email is already in use!");
+        }
+
+        callback();
+    });
+};
+
+router.post('/verify', (req, res) => {
+    let token = req.headers["x-access-token"];
+
+    if (!token) {
+        return res.status(403).send({ message: "No token provided!" });
+    }
+
+    jwt.verify(token, config.secret, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: "Unauthorized!" });
+        }
+        req.userId = decoded.id;
+    });
+});
+
+router.post('/login', (req, res) => {
+    User.findOne({
+        email: req.body.email
+    })
+        .exec((err, user) => {
+            if (err) {
+                res.status(500).send({ message: err });
+                return;
+            }
+
+            if (!user) {
+                return res.status(404).send({ message: "User Not found." });
+            }
+
+            var passwordIsValid = bcrypt.compareSync(
+                req.body.password,
+                user.password
+            );
+
+            if (!passwordIsValid) {
+                return res.status(401).send({
+                    accessToken: null,
+                    message: "Invalid Password!"
+                });
+            }
+
+            var token = jwt.sign({ id: user.id }, config.secret, {
+                expiresIn: 86400 // 24 hours
+            });
+
+            res.status(200).send({
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                date: user.date,
+                accessToken: token
+            });
+        });
+});
+
+router.post('/register', (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        date: Date.now,
+        password: bcrypt.hashSync(req.body.password, 8)
+    });
+
+    fail = err => res.status(500).send({message: err});
+
+    check_duplicate(user.email, () => user.save((err, user) => {
+        err ? fail(err) : res.send({ message: "User was registered successfully!" });
+    }), fail);
+});
 
 module.exports = router;
