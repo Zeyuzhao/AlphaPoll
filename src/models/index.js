d47f8f3c178ed571c16db660b21158ef20c92dea
Simon Chervenak <sarras305@gmail.com>
2020-12-05 21:54:52 -0500

    fix require statements

--- src/models/index.js
+++ src/models/index.js
@@ -5,9 +5,6 @@ const db = {};
 
 db.mongoose = mongoose;
 
-db.user = require("./user.model");
-db.role = require("./role.model");
-
-db.ROLES = ["user", "admin", "moderator"];
+db.user = require("./user.js");
 
 module.exports = db;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const db = require("./app/models");
+const db = require("./models");
 const config = require("./models/config.js");
 
 const app = express();
