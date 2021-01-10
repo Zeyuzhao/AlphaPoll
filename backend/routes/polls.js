dc4e6fe2d180f8877c1e2b7d917e5e828276e6e9
Zach Zhao <thezachzhao@gmail.com>
2021-01-10 00:07:46 -0500

    Add shortid to polls

--- backend/routes/polls.js
+++ backend/routes/polls.js
@@ -4,13 +4,14 @@ const Poll = require("../model/Poll");
 const verify = require("../utils/verifyToken");
 const { pollValidation } = require("../utils/validation");
 
-// Get all posts under user
+// Get all polls under user
 router.get("/", verify, async (req, res) => {
   const userId = req.user["_id"];
   const polls = await Poll.find({owner: userId}).exec();
   res.send(polls);
 });
 
+// Create a new poll
 router.post("/", verify, async (req, res) => {
   const userId = req.user["_id"];
   // Validate data before poll creation
@@ -20,7 +21,7 @@ router.post("/", verify, async (req, res) => {
   {
     name: "test",
     questions: [{prompt: "You lose: ", response: ["a", "b", "c"]}],
-    owner: "_id"
+    owner: "user._id"
   }
   */
   const poll = new Poll(req.body);
@@ -29,8 +30,41 @@ router.post("/", verify, async (req, res) => {
     const savedPoll = await poll.save();
     res.send({ poll: savedPoll._id });
   } catch (err) {
-    res.status(400).send(err);
+    // Server Error
+    res.status(500).send(err);
   }
 });
-// router.get("/")
+
+// Get poll info: public
+// TODO: Make a private and non-private route
+router.get("/:shortId", async (req, res) => {
+  const {name, questions} = await Poll.findOne({shortId: req.params["shortId"]}).exec();
+  const poll = {name, questions};
+  console.log(poll);
+  res.send(poll);
+});
+
+router.delete("/:shortId", verify, async (req, res) => {
+  
+  const userId = req.user["_id"];
+  const poll = await Poll.findOne({shortId: req.params["shortId"]}).exec();
+  
+  // Check that user owns poll
+  if (userId !== String(poll.owner)) {
+    return res.status(401).send({
+      success: false,
+      msg: "User _ is not owner of poll"
+    });
+  }
+
+  try {  
+    await Poll.deleteOne({shortId: req.params["shortId"]}).exec();
+    res.send({ success: true });
+  } catch(err) {
+    // Server Error
+    res.status(500).send(err);
+  }
+});
+
+
 module.exports = router;
